effa28c1ac65cd43c8604d406416e8fe441de96c
gabitheiss <t_gaaby@hotmail.com>
2021-08-13 10:58:26 -0300

    Ajuste

--- app/src/main/java/com/example/sharedpreferences/model/Name.kt
+++ app/src/main/java/com/example/sharedpreferences/model/Name.kt
@@ -3,6 +3,6 @@ package com.example.sharedpreferences.model
 import com.google.gson.annotations.SerializedName
 
 data class Name(
-    @SerializedName ("firtsname") val firtsname: String,
+    @SerializedName ("firstname") val firstname: String,
     @SerializedName("lastname") val lastname: String
 )
